8f8837ce2b7f146e7b49031980a5b1c9311fe53d
gavieira <gabrieldeusdeth@gmail.com>
2020-06-22 23:05:17 -0300

    Symlinks created at the end of script, after removing previous dotfiles

--- setup.sh
+++ setup.sh
@@ -12,35 +12,13 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-echo "removing previous dotfiles:"
-rm $HOME/.vimrc
-rm $HOME/.zshrc #Removes previous .zshrc file
-rm $HOME/.p10k.zsh
-rm $HOME/.tmux.conf
-
-echo "creating symbolic links in $HOME:"
-
-# create symbolic link to this repo's .vimrc file
-ln -s $SCRIPTPATH/.vimrc ~/.vimrc
-echo ".vimrc symlink created"
-
 # copy this repo's .vim dir
 cp -r $SCRIPTPATH/.vim $HOME/.vim
 echo ".vim directory copied"
 
-# Installing vim plugins:
-vim +'PlugInstall --sync' +qa
-#vim -E -s -u "path_to_vim_rc/.vimrc" +PlugInstall +qall # From https://github.com/junegunn/vim-plug/issues/225#issuecomment-458710986
-echo "vim plugins installed"
-
-# Create symbolic link to repo's .tmux_conf file
-ln -s $SCRIPTPATH/.tmux.conf $HOME/.tmux.conf
-echo ".tmux.conf symlink created"
-
 # Install .oh-my-zsh
 echo "Installing oh-my-zsh"
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" --skip-chsh --unattended --keep-zshrc
-
 
 
 # Installing plugins/themes for zsh shell:
@@ -48,7 +26,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.
 
 #zsh syntax highlighting:
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 
 
 # INSTALL FONTS AND POWERLEVEL10k
@@ -68,12 +45,32 @@ if [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then
 	        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 fi
 
+echo "creating symbolic links in $HOME:"
+
+# create symbolic link to this repo's .vimrc file
+rm $HOME/.vimrc
+ln -s $SCRIPTPATH/.vimrc ~/.vimrc
+echo ".vimrc symlink created"
+
+# Installing vim plugins:
+vim +'PlugInstall --sync' +qa
+#vim -E -s -u "path_to_vim_rc/.vimrc" +PlugInstall +qall # From https://github.com/junegunn/vim-plug/issues/225#issuecomment-458710986
+echo "vim plugins installed"
+
+# Create symbolic link to repo's .tmux_conf file
+rm $HOME/.tmux.conf
+ln -s $SCRIPTPATH/.tmux.conf $HOME/.tmux.conf
+echo ".tmux.conf symlink created"
+
 # Create symbolic link to .p10k.zsh
+rm $HOME/.p10k.zsh
 ln -s $SCRIPTPATH/.p10k.zsh $HOME/.p10k.zsh
 echo ".p10k.zsh symlink created"
 
 # Create symbolic link to repo's .zshrc file
+rm $HOME/.zshrc #Removes previous .zshrc file
 ln -s $SCRIPTPATH/.zshrc $HOME/.zshrc 
 echo ".zshrc symlink created"
+
 
 echo "Finished!!"
