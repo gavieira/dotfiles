1a1a9fcfd8718f99c0d86f4720bab7562f7ef40f
gavieira <gabrieldeusdeth@gmail.com>
2021-02-26 17:44:41 -0300

    added streamlink config and updated youtube_watch.sh to use it to open twitch streams

--- .config/youtube_watch.sh
+++ .config/youtube_watch.sh
@@ -2,5 +2,11 @@
 # originally by Kris Occhipinti
 # https://www.youtube.com/watch?v=FsQuGplQvrw
 
-dunstify "Playing Video" "$(xclip -o)";
-mpv "$(xclip -o)"
+notify-send -t 3000 "Playing Video" "$(xclip -o)";
+
+if [[ $(xclip -o) == https://www.twitch.tv* ]] 
+then
+  streamlink --twitch-disable-ads "$(xclip -o)" 720p60 || mpv "$(xclip -o)"
+else
+  mpv "$(xclip -o)"
+fi
