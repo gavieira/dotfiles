83084eef3d7b125a02536d91348469832356a624
gavieira <gabrieldeusdeth@gmail.com>
2021-02-27 10:48:39 -0300

    streamlink config for twitch and youtube plugins
    mpv input.conf and youtube_watch modified as well

--- .config/youtube_watch.sh
+++ .config/youtube_watch.sh
@@ -6,7 +6,11 @@ notify-send -t 3000 "Playing Video" "$(xclip -o)";
 
 if [[ $(xclip -o) == https://www.twitch.tv* ]] 
 then
-  streamlink --twitch-disable-ads "$(xclip -o)" 720p60 || mpv "$(xclip -o)"
+  streamlink "$(xclip -o)" || mpv "$(xclip -o)"
 else
   mpv "$(xclip -o)"
 fi
+
+#This should work as well (no need for the if)
+#First tries to open with streamlink. If it fails, uses mpv as fallback option.
+#streamlink "$(xclip -o)" || mpv "$(xclip -o)"
