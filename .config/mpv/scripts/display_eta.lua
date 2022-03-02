4c125f2734213e87d51f7b599a06514e6a8e024c
gavieira <gabrieldeusdeth@gmail.com>
2022-03-02 12:12:46 -0300

    Changed mpv script 'display_eta.lua' to show playtime remaining
    Changed default app for 'application/pdf' to correct okular .desktop file

--- .config/mpv/scripts/display_eta.lua
+++ .config/mpv/scripts/display_eta.lua
@@ -26,7 +26,7 @@ function display_eta()
 		local format_string = same_day and "%H:%M %p" or "%H:%M %p (%A)"
 		local formatted_time = os.date(format_string, end_time)
 
-		mp.osd_message("Speed: " .. playback_speed .."x" .. "\nSystem Time: " .. os.date("%H:%M %p") ..  "\nETA: " .. formatted_time, 5)
+		mp.osd_message("Speed: " .. playback_speed .."x" .. "\nSystem Time: " .. os.date("%H:%M %p") ..  "\nETA: " .. formatted_time .. "\nPlaytime remaining: " .. mp.get_property_osd("playtime-remaining"))
 	else
 		mp.osd_message("Unable to estimate ending time")
 	end
