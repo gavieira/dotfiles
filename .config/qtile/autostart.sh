efaa20852309d5d8daf44e1824cc3dd4367d852d
gavieira <gabrieldeusdeth@gmail.com>
2024-02-19 20:59:36 -0300

    Added transmission to qtile startup applications

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,6 +3,7 @@
 dropbox start &
 nm-applet &
 ./.config/switch_monitors.sh &
+transmission-gtk --minimized &
 #setxkbmap -option caps:super & #set CAPS to be an alternative super key
 xmodmap -e "keycode 135 = Super_L" & #Rebinds the "Menu" key as an alternative super key
 
