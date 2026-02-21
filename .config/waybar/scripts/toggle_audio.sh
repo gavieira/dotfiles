87b9df07e0cfcdfb23fd26e16667e834ad2e5743
gavieira <gabrieldeusdeth@gmail.com>
2026-02-20 22:59:11 -0300

    updated caps keybinding in hypr and max pulseaudio volume in waybar

--- .config/waybar/scripts/toggle_audio.sh
+++ .config/waybar/scripts/toggle_audio.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-# Nomes curtos baseados no seu pactl list
+# Identificadores extraídos do seu sistema
 SINK_HDMI="alsa_output.pci-0000_03_00.1.pro-output-7"
-SINK_MCHOSE="alsa_output.usb-C-Media_Electronics_Inc._MCHOSE_V9_PRO_0123456789AB-01.pro-output-0"
+SINK_MCHOSE_NORMAL="alsa_output.usb-C-Media_Electronics_Inc._MCHOSE_V9_PRO_0123456789AB-01.pro-output-0"
+SINK_MCHOSE_HEADSET="alsa_output.usb-C-Media_Electronics_Inc._MCHOSE_V9_PRO_HEADSET_0123456789AB-01.analog-stereo"
 
 # Pega o dispositivo padrão atual
 CURRENT_SINK=$(pactl get-default-sink)
 
-if [ "$CURRENT_SINK" = "$SINK_MCHOSE" ]; then
-    pactl set-default-sink "$SINK_HDMI"
-    notify-send "Áudio" "Saída alterada para: HDMI/DisplayPort" -i audio-speakers
+# Verifica se o Headset está fisicamente conectado/disponível no PipeWire
+HEADSET_PLUGADO=$(pactl list short sinks | grep "MCHOSE_V9_PRO_HEADSET")
+
+if [ "$CURRENT_SINK" = "$SINK_HDMI" ]; then
+    # Se estou no HDMI e o Headset estiver disponível, ele é a prioridade
+    if [ -n "$HEADSET_PLUGADO" ]; then
+        pactl set-default-sink "$SINK_MCHOSE_HEADSET"
+        notify-send "Áudio" "🎧 Headset MCHOSE Conectado" -i audio-headphones -t 2000
+    else
+        # Se não tem headset, tenta a saída normal do MCHOSE
+        pactl set-default-sink "$SINK_MCHOSE_NORMAL"
+        notify-send "Áudio" "🔊 MCHOSE V9 PRO Ativado" -i audio-speakers -t 2000
+    fi
 else
-    pactl set-default-sink "$SINK_MCHOSE"
-    notify-send "Áudio" "Saída alterada para: MCHOSE V9 PRO" -i audio-headphones
+    # Se eu já estiver em qualquer uma das saídas MCHOSE, volta para o HDMI
+    pactl set-default-sink "$SINK_HDMI"
+    notify-send "Áudio" "🖥️ Saída HDMI/Monitor Ativada" -i video-display -t 2000
 fi
